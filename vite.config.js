da972ba1f733043f23aadccf20a09ce7ad161cd5
wm <1920754788@qq.com>
2025-11-21 00:21:54 +0800

    打包目录更新为/docs

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
@@ -7,5 +9,7 @@ export default defineConfig({
     host: '0.0.0.0', // 监听所有网络接口，允许通过 IP 地址访问
     port: 5173, // 指定端口号
   },
+  build: {
+    outDir: 'docs'
+  }
 })
-
